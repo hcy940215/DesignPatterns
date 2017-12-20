df3fc2976b28dfe2986ed7a27fcec6733137c84b
hcy940215 <894442036@qq.com>
2017-12-20 14:37:48 +0800

    Builde模式练习

--- builderpattern/src/test/java/com/shengmingji/designpatterns/Builder.java
+++ builderpattern/src/test/java/com/shengmingji/designpatterns/Builder.java
@@ -6,7 +6,14 @@ package com.shengmingji.designpatterns;
 
 public abstract class Builder {
     public abstract Builder setBoard(String board);
+
     public abstract Builder setDisplay(String display);
+
     public abstract Builder setOS();
+
     public abstract Computer create();
+
+//    public abstract void builderBoard(String board);
+//    public abstract void builderDisplay(String display);
+//    public abstract void builderOS();
 }
